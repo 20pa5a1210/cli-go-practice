6bffd0e57a1c31e0b0ad65799e3e4ad208caf9d3
20pa5a1210 <20pa5a1210@vishnu.edu.in>
2023-06-09 16:48:09 +0530

    fix: done panic fixed when index out of bound

--- todo-cli-go/commands/command_done.go
+++ todo-cli-go/commands/command_done.go
@@ -12,6 +12,10 @@ func Done_command(cfg *TodoList, args ...string) error {
 		return nil
 	}
 	taskNum := int(args[0][0] - '0')
+	if taskNum > len(cfg.Tasks) {
+		fmt.Println("> Task number does not exist")
+		return nil
+	}
 	cfg.Tasks[taskNum].Done = true
 	fmt.Printf("Marked task %d as done\n", taskNum)
 	return nil
